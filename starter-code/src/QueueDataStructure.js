6bb808c5ef579a7da083faa9cfce648cfd5362c0
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-08-24 15:02:49 +0200

    First and second iteration done. Now, visual part.

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,46 @@
+
 function QueueDataStructure () {
+  this.queueControl = [];
+  this.MAX_SIZE = 3;
+  this.isEmpty = function (){
+    if (this.queueControl.length===0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  this.canEnqueue = function(){
+    if (this.queueControl.length>=this.MAX_SIZE) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  };
+
+  this.enqueue = function(element){
+
+    if (this.queueControl.length===this.MAX_SIZE) {
+      return "Queue Overflow";
+    }
+    else {
+      this.queueControl.unshift(element);
+      return this.queueControl;
+    }
+  };
+
+
+  this.dequeue = function(element){
+    if (this.queueControl.length === 0) {
+      return "Queue Underflow";
+    }
+    else {
+      return this.queueControl.pop(element);
+    }
+  };
+
 }
+
+var queue = new QueueDataStructure();
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,38 @@
 function StackDataStructure () {
+
+  this.stackControl = [];
+  this.MAX_SIZE = 3;
+  this.isEmpty = function(){
+    return this.stackControl.length === 0;
+  };
+  this.canPush = function(){
+    if (this.stackControl.length>=this.MAX_SIZE) {
+      return false;
+    }
+    else {
+      return true;
+    }
+
+  };
+  this.push = function(element){
+
+    if (this.stackControl.length===this.MAX_SIZE) {
+      return "Stack Overflow";
+    }
+    else {
+      this.stackControl.push(element);
+      return this.stackControl;
+    }
+  };
+  this.pop = function(){
+    if (this.stackControl.length === 0) {
+      return "Stack Underflow";
+    }
+    else {
+      return this.stackControl.pop();
+    }
+  };
 }
+
+StackDataStructure.push(19);
+StackDataStructure.push(88);
